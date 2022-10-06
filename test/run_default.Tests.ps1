9255311740ff020118bec15e9fde7d047b25f797
Edward van de Vorst <edward.van.de.vorst@philips.com>
2022-10-06 11:48:15 +0200

    chore: use mapping path

--- test/run_default.Tests.ps1
+++ test/run_default.Tests.ps1
@@ -2,14 +2,14 @@ Describe "run_default" {
     Context "when no workspace, mapping, work path is used" {
         It "current directory should be github workspace path" {
             $currentdir = "$pwd"
-            $github_workspace = "$env:GITHUB_WORKSPACE"
-            Write-Output $currentdir | Should -Be $github_workspace
+            $github_workspace = "$env:TEST_MAPPING_PATH"
+            Write-Output $currentdir | Should -BeLike $github_workspace
         }
     }
 
     Context "when no workspace, mapping, work path is used" {
         It "work directory directory should point to github workspace path" {
-            $testfile = "$env:GITHUB_WORKSPACE" + "\test\run_default.Tests.ps1"
+            $testfile = "$env:TEST_MAPPING_PATH" + "\test\run_default.Tests.ps1"
             $testfile | Should -Exist
         }
     }
