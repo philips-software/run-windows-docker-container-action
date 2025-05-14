01d5ac765c1251387623bcde1a0913671aaa70c5
Edward van de Vorst <edward.van.de.vorst@philips.com>
2025-05-14 11:40:04 +0200

    chore: fix tests

--- test/run_test.ps1
+++ test/run_test.ps1
@@ -10,7 +10,7 @@ if (-not(Test-Path -Path $testPath -PathType Leaf))
 
 Install-PackageProvider NuGet -Force;
 If ($? -ne "True" ) { Throw };
-Install-Module -Force Pester;
+Install-Module -Force -SkipPublisherCheck Pester;
 If ($? -ne "True" ) { Throw };
 
 $config=New-PesterConfiguration;
